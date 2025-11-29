f42e8e3712200389e2857dc38db87c8f67bf9ff6
tl1l1l1s <s2oo1og24@gmail.com>
2025-11-29 14:29:48 +0900

    Feat: React-router를 통한 route 설정

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,26 @@ import "./App.css";
 import Layout from "./components/layout/Layout";
 import { Route, Routes } from "react-router-dom";
 import Home from "./pages/Home";
+import Login from "./pages/Login";
+import My from "./pages/My";
+import Join from "./pages/Join";
+import ProfileEdit from "./pages/ProfileEdit";
+import ArticleWrite from "./pages/ArticleWrite";
+import ArticleEdit from "./pages/ArticleEdit";
+import ArticleDetail from "./pages/ArticleDetail";
 
 function App() {
   return (
     <Layout>
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/join" element={<Join />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/my" element={<My />} />
+        <Route path="/my/change" element={<ProfileEdit />} />
+        <Route path="/articles/write" element={<ArticleWrite />} />
+        <Route path="/articles/:id/edit" element={<ArticleEdit />} />
+        <Route path="/articles/:id" element={<ArticleDetail />} />
       </Routes>
     </Layout>
   );
---
+++ src/components/SearchBar.jsx
@@ -0,0 +1,45 @@
+import styles from "./SearchBar.module.css";
+import searchIcon from "../../public/assets/images/search.svg";
+import Theme from "./common/Theme";
+
+export default function SearchBar() {
+  return (
+    <>
+      <div className={styles.searchBox}>
+        <div className={styles.search} id="header-search">
+          <img className={styles.searchIcon} src={searchIcon} />
+          <input
+            className={styles.input}
+            type="text"
+            id="search"
+            name="search"
+            placeholder="검색하기..."
+            minLength="1"
+            autoComplete="off"
+          />
+        </div>
+        <div className={styles.dropdown} id="search-dropdown">
+          <div className="search-theme">
+            <div className={styles.themeContainer} id="search-theme-list">
+              <Theme theme="All" isActive={true} />
+              <Theme theme="Dog" isActive={false} />
+              <Theme theme="Cat" isActive={false} />
+              <Theme theme="Coffee" isActive={false} />
+              <Theme theme="Dinner" isActive={false} />
+              <Theme theme="Other" isActive={false} />
+            </div>
+          </div>
+          <hr className={styles.dropdownDivider} />
+          <div className="popular">
+            <div className={styles.popularTitle}>인기 검색어</div>
+            <div className={styles.popularList} id="popular-list">
+              <div className={styles.popularItem}>1. 강아지</div>
+              <div className={styles.popularItem}>2. 고양이</div>
+              <div className={styles.popularItem}>3. 헤드셋</div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+}
