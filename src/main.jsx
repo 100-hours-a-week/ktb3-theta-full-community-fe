7474b7610dfa9c92540ee113c9df52ee37b3a127
tl1l1l1s <s2oo1og24@gmail.com>
2025-11-30 22:51:26 +0900

    Feat: Tanstack-Query 기초 세팅

--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,16 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
-import "./global.css";
 import App from "./App.jsx";
+import { queryClient } from "./api/queryClient.js";
+import "./global.css";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
+    <QueryClientProvider client={queryClient}>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </QueryClientProvider>
   </StrictMode>
 );
